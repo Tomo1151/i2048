386129ead80a531cd8e4e93755354d835cc83940
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-06-17 14:50:22 +0900

    4×4の枠と背景を作った

--- i2048/ContentView.swift
+++ i2048/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var BOARD: [Int?] = [
+        4, nil, nil, nil,
+        nil, nil, nil, 2,
+        nil, nil, nil, nil,
+        nil, nil, nil, nil,
+    ]
+    
+    let BOARD_SIZE: Int = 4
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color("body_bg").edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ForEach(0..<BOARD_SIZE, id: \.self) { i in
+                    HStack {
+                        ForEach(0..<BOARD_SIZE, id: \.self) { j in
+                            Text("\(BOARD[i * BOARD_SIZE + j] != nil ? "\(BOARD[i * BOARD_SIZE + j]!)" : "")")
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .padding(12)
+                                .background(Color("cell_bg_empty"))
+                                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                                .shadow(color: .brown, radius: 3, x: 2, y: 2)
+                                .fontWeight(.heavy)
+                        }
+                    }
+                }
+            }
+            .padding(10)
+            .background(Color("board_bg"))
+            .fontDesign(.monospaced)
+            .clipShape(RoundedRectangle(cornerRadius: 26, style: .continuous))
         }
-        .padding()
     }
 }
 
